16e6ab8b65117b63368fcb503aad6cd643b6488a
polsterc16 <christoph.polster@edu.fh-joanneum.at>
2025-03-11 09:35:23 +0100

    start of binary counter

---
+++ proj_master_2025_lib/hdl/c_001_binary_counter_rtl.vhd
@@ -0,0 +1,21 @@
+--
+-- VHDL Architecture proj_master_2025_lib.c_001_binary_counter.rtl
+--
+-- Created:
+--          by - Admin.UNKNOWN (LAPTOP-7KFJT032)
+--          at - 08:57:36 11.03.2025
+--
+-- using Mentor Graphics HDL Designer(TM) 2017.1a (Build 5)
+--
+LIBRARY ieee;
+USE ieee.std_logic_1164.all;
+USE ieee.numeric_std.all;
+
+entity c_001_binary_counter is
+end entity c_001_binary_counter;
+
+--
+architecture rtl of c_001_binary_counter is
+begin
+end architecture rtl;
+
